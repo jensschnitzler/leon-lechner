20396ecf14e86b4126d301fc55cc1caa469cd255
jensschnitzler <jens.schnitzler@gmail.com>
2023-03-10 17:02:11 +0000

    update

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Leon Lechner</title>
+	<meta name="description" content="Hi, I’m Leon Lechner, an artist and graphic designer based in Hamburg and Berlin. In various collaborations I develop my artistic practice between collective process, media art, performative action, artistic research, critical spatial practice and mediation. As an independent graphic designer I work e.g. on books, websites, identities, posters and exhibitions.">
+	<meta name="author" content="Leon Lechner">
+
+	<meta property="og:title" content="Leon Lechner">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="">
+
+	<link rel="icon" href="/favicon.ico">
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+	<link rel="stylesheet" href="css/styles.css?v=1.0">
+	
+	<style>
+		span.obfuscate { display: none; }
+	</style>
+
+</head>
+
+<body>
+	
+	<p>Hi, I’m Leon Lechner, an artist and graphic designer based in Hamburg and Berlin. In various collaborations I develop my artistic practice between collective process, media art, performative action, artistic research, critical spatial practice and mediation. As an independent graphic designer I work e.g. on books, websites, identities, posters and exhibitions.</p>
+	
+	<p>Feel free to get in touch for further work enquiries and collaborations.</p>
+	
+	<p><a href="https://www.instagram.com/lnlchnr/" target="_blank">@lnlchnr</a></p>
+	
+	<p>
+		<script language="JavaScript">
+			user = 'name';
+			site = 'domain';
+			document.write('<a href=\"mailto:' + user + '@' + site + '\" rel=\"nofollow\">');
+			document.write(user + '<span class=\"obfuscate\">null</span>@' + site + '</a>');
+		</script>
+	</p>
+
+
+	<!-- your content here... -->
+	<script src="js/scripts.js"></script>
+</body>
+</html>
